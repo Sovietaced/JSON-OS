3aa85b6cf60f2919168bba6c509416f8b28131ed
Sovietaced <Sovietaced@gmail.com>
2013-10-03 01:28:06 -0700

    container for PCB

---
+++ scripts/os/processControlBlock.js
@@ -0,0 +1,22 @@
+/* ------------  
+   processControlBlock.js
+
+   Requires global.js.
+   
+   ------------ */
+
+function ProcessControlBlock() {
+
+	var pointer = null;
+    var processState = null;  
+    var pid = null;
+    var PC = 0;
+    var PS = null;
+    var memoryInfo = null;               
+    
+    
+    this.init = function() {
+        
+    };
+    
+}
