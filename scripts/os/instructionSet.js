3876bcb17138227b5cd7e65fd2208378429e8af2
Sovietaced <Sovietaced@gmail.com>
2013-10-08 16:06:15 -0700

    added all 6502 commands, they still need work and need to be tested

--- scripts/os/instructionSet.js
+++ scripts/os/instructionSet.js
@@ -5,26 +5,75 @@
  * ----------------- */
 
 var _OpCodes = {
-    'A9': { args: 2, funct: loadWithConstant },
-    'AD': { args: 4, funct: loadFromMemory },
-    '8D': { args: 4, funct: loadWithConstant },
-    '6D': { args: 4, funct: loadWithConstant },
-    'A2': { args: 2, funct: loadWithConstant },
-    'AE': { args: 4, funct: loadWithConstant },
-    'A0': { args: 2, funct: loadWithConstant },
-    'AC': { args: 4, funct: loadWithConstant },
-    'EA': { args: 0, funct: loadWithConstant },
-    '00': { args: 0, funct: loadWithConstant },
-    'EC': { args: 4, funct: loadWithConstant },
-    'D0': { args: 2, funct: loadWithConstant },
-    'EE': { args: 4, funct: loadWithConstant },
-    'FF': { args: 0, funct: loadWithConstant }
+    'A9': { args: 2, funct: loadAccWithConstant },
+    'AD': { args: 4, funct: loadAccFromMemory },
+    '8D': { args: 4, funct: storeAccInMemory },
+    '6D': { args: 4, funct: addWithCarry },
+    'A2': { args: 2, funct: loadXregWithConstant },
+    'AE': { args: 4, funct: loadXregFromMemory },
+    'A0': { args: 2, funct: loadYregWithContant },
+    'AC': { args: 4, funct: loadYregFromMemory },
+    'EA': { args: 0, funct: noOp },
+    '00': { args: 0, funct: brk },
+    'EC': { args: 4, funct: compare },
+    'D0': { args: 2, funct: branch },
+    'EE': { args: 4, funct: increment },
+    'FF': { args: 0, funct: system }
 };
 
-function loadWithConstant(constant){
+function loadAccWithConstant(constant){
     _CPU.Acc = constant;
 }
 
-function loadFromMemory(PC){
+function loadAccFromMemory(PC){
     _CPU.Acc = _RAM.readMemory(PC);
+}
+
+function storeAccInMemory(PC){
+    _CPU.Acc = _RAM.readMemory(PC);
+}
+
+function addWithCarry(PC){
+    //herp
+}
+
+function loadXregWithConstant(constant){
+    _CPU.Xreg = constant;
+}
+
+function loadXregFromMemory(PC){
+    _CPU.Xreg = _RAM.readMemory(PC);
+}
+
+function loadYregWithConstant(constant){
+    _CPU.Yreg = constant;
+}
+
+function loadYregFromMemory(PC){
+    _CPU.Yreg = _RAM.readMemory(PC);
+}
+
+function noOp(){
+    //wot
+}
+
+function brk(){
+    _//wot
+}
+
+function compare(PC){
+    var value = _RAM.readMemory(PC);
+    if (value == _CPU.Xreg){
+        _CPU.Zflag = true;
+    }
+}
+
+function branch(numBytes){
+    if( _CPU.Zflag == 0){
+        _CPU.PC += numBytes;
+    }
+}
+
+function system(){
+    //derp
 }
