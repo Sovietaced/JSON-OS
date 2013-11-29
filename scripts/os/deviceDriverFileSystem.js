742db79a0117600d049be8420233e89bc1c32597
Sovietaced <Sovietaced@gmail.com>
2013-11-28 17:24:54 -0800

    some contants, slowly proceeding and thinking about how to implement this

--- scripts/os/deviceDriverFileSystem.js
+++ scripts/os/deviceDriverFileSystem.js
@@ -7,8 +7,22 @@
    ---------------------------------- */
 
 DeviceDriverFileSystem.prototype = new DeviceDriver;  // "Inherit" from prototype DeviceDriver in deviceDriver.js.
- //These are special cases that don't fit the ASCII mapping
 
+var disk = null;
+
+var directory = null;
+
+// Constants
+var TRACK_SIZE = 4;
+var SECTOR_SIZE = 8;
+var BLOCK_SIZE = 8;
+
+var TRACKS = {
+     DIRECTORY_DATA : [0],
+     FILE_DATA : [1,2,3]
+}
+
+var MBR_TSB = "000"
 
 function DeviceDriverFileSystem()                     // Add or override specific attributes and method pointers.
 {
@@ -23,6 +37,8 @@ function DeviceDriverFileSystem()                     // Add or override specifi
 function krnFSDriverEntry()
 {
     // Initialization routine for this, the kernel-mode File System Device Driver.
+    disk = _HDD;                                // Assign Hardawre to driver
+    krnFSFormat();                              // Format the disk
     this.status = "loaded";
     // More?
 }
@@ -30,4 +46,9 @@ function krnFSDriverEntry()
 function krnFSDispatchDiskRequest(params)
 {
    
+}
+
+function krnFSFormat()
+{
+    directory = {}                               // Instantiate clean direcotry data
 }
