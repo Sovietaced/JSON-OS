66458caa267cdc45ce954fc022f1589ec60d2f04
Sovietaced <Sovietaced@gmail.com>
2013-11-28 20:39:37 -0800

    got find free block function working

--- scripts/os/deviceDriverFileSystem.js
+++ scripts/os/deviceDriverFileSystem.js
@@ -19,15 +19,15 @@ var NUM_BLOCKS = 8;
 var BLOCK_SIZE = 64;
 
 var TRACKS = {
-     DIRECTORY_DATA : [0],
+     DIRECTORY_DATA : 1,
      FILE_DATA : [1,2,3]
 }
 
 var BLOCKS = {
-    ACTIVITY_INDEX : [0],
-    TRACK_INDEX : [1],
-    SECTOR_INDEX : [2],
-    BLOCK_INDEX : [3],
+    ACTIVITY_INDEX : 0,
+    TRACK_INDEX : 1,
+    SECTOR_INDEX : 2,
+    BLOCK_INDEX : 3,
     DATA_INDEX : [4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,
     36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63]
 }
@@ -69,10 +69,35 @@ function generateDiskData(activity, t, s, b, data)
     if (data == ''){ 
         data = Array(61).join("0")
     }
-
-    console.log(data);
     // Convert values to strings, map get ASCII calues to hex strings
     return (activity.toString() + TSB + data.toString()).split ('').map (function (c) { return c.charCodeAt(0).toString(16); })
+}
+
+function decodeDiskData(dataa)
+{   
+    // Convert data to hex byte array
+    dataa = dataa.split(',')
+    // Convert from Hex to String value
+    for (var key in dataa){
+        dataa[key] = String.fromCharCode(hexToInt(dataa[key]));
+    }    
+
+    // Gather values
+    var activity = dataa[BLOCKS.ACTIVITY_INDEX];
+    var track = dataa[BLOCKS.TRACK_INDEX];
+    var block = dataa[BLOCKS.BLOCK_INDEX];
+    var sector = dataa[BLOCKS.SECTOR_INDEX];
+    var data = '';
+    for (var index = 0; index < BLOCKS.DATA_INDEX.length; index++){
+        data += dataa[BLOCKS.DATA_INDEX[index]];
+    }    
+
+    // return hash of values
+    return { 'activity' : activity,
+            'track' : track, 
+            'block' : block,
+            'sector': sector,
+            'data' : data}
 }
 
 function krnFSDispatchDiskRequest(params)
@@ -93,11 +118,12 @@ function krnFSFormat()
                 var tsb = generateTSB(t,s,b);
                 var data = generateDiskData(0, '-', '-', '-', "");
 
-                // Write MBR to MBR tsb
+                // Generate MBR code
                 if (tsb === MBR_TSB){
                     data = generateDiskData(1, '-', '-', '-', "MBR");
                 }
 
+                // Write data to disk
                 disk.write(tsb,data);
             }
         }
@@ -114,14 +140,21 @@ function krnCreateFile(name, data)
 
 function findFreeBlock()
 {
-    for (track in TRACKS.DIRECTORY_DATA){
-         for (var s = 0; s < NUM_SECTORS; s++){
+    for (var t = 0; t < TRACKS.FILE_DATA.length; t++){
+        var track = TRACKS.FILE_DATA[t];
+        for (var s = 0; s < NUM_SECTORS; s++){
             for (var b = 0; b < NUM_BLOCKS; b++){
 
                 var tsb = generateTSB(track,s,b);
                 var data = disk.read(tsb);
 
+                // Get value hash
+                data = decodeDiskData(data);
 
+                // Check for non active
+                if (data['activity'] == '0'){
+                    return tsb;
+                }
             }
         }
     }
